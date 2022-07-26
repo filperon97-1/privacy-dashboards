06d3473dc69ae1989878217104fb3b5cd9d4e724
FILIPPO PERON s284138 <s284138@studenti.polito.it>
2022-07-26 23:45:43 +0200

    Azioni per tutti i Right, da migliorare

--- src/main/java/com/privacydashboard/application/views/rights/DialogRight.java
+++ src/main/java/com/privacydashboard/application/views/rights/DialogRight.java
@@ -11,11 +11,13 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Header;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 
+import java.util.LinkedList;
 import java.util.Optional;
 
 public class DialogRight{
@@ -23,7 +25,7 @@ public class DialogRight{
     private final AuthenticatedUser authenticatedUser;
 
     private final Dialog requestDialog= new Dialog();
-    private final H1 title=new H1();
+    private final Header title=new Header();
     private final HorizontalLayout content=new HorizontalLayout();
     private final Button cancelButton=new Button("Cancel", e-> requestDialog.close());
     private final Button continueButton=new Button("Continue");
@@ -46,6 +48,12 @@ public class DialogRight{
         if(request.getRightType().equals(RightType.ERASURE)){
             erasure(user, request);
         }
+        if(request.getRightType().equals(RightType.INFO)){
+            info(user, request);
+        }
+        if(request.getRightType().equals(RightType.COMPLAIN)){
+            complain(user, request);
+        }
         requestDialog.open();
     }
 
@@ -53,6 +61,7 @@ public class DialogRight{
         HorizontalLayout buttonLayout= new HorizontalLayout(continueButton, cancelButton);
         VerticalLayout layout=new VerticalLayout(title, content, buttonLayout);
         requestDialog.add(layout);
+        //requestDialog.setWidth("70%");
     }
 
     private RightRequest initializeRequest(User user, RightType rightType){
@@ -96,6 +105,7 @@ public class DialogRight{
 
         TextArea textArea=new TextArea("What to erase");
         textArea.setPlaceholder("Descrive what you want to erase");
+        textArea.setWidthFull();
 
         continueButton.addClickListener(e->{
             if(appComboBox.getValue()!=null && textArea.getValue()!=null){
@@ -107,6 +117,61 @@ public class DialogRight{
             }
         });
         content.add(appComboBox, textArea);
+    }
+
+    private void info(User user, RightRequest request){
+        title.setText("Select App");
+
+        ComboBox<IoTApp> appComboBox= new ComboBox<>("Apps");
+        appComboBox.setItems(dataBaseService.getUserApps(user));
+        appComboBox.setItemLabelGenerator(IoTApp::getName);
+        appComboBox.setPlaceholder("Filter by name...");
+
+        ComboBox<String> infoComboBox= new ComboBox<>("info");
+        LinkedList<String> infos= new LinkedList<>();
+        infos.add("Periodo di mantenimento dei dati");
+        infos.add("Contatto del Data Protection Officer");
+        infos.add("Conoscere lo scopo del processamento dei dati e le sue basi legali");
+        infos.add("Conoscere gli interessi legittimi del Controller o delle terze parti");
+        infos.add("Conoscere i destinatari dei dati");
+        infos.add("Sapere se i dati vanno a paesi terzi");
+        infos.add("Altro");
+        infoComboBox.setItems(infos);
+
+        continueButton.addClickListener(e->{
+            if(appComboBox.getValue()!=null && infoComboBox.getValue()!=null){
+                request.setApp(appComboBox.getValue());
+                request.setReceiver(dataBaseService.getControllersFromApp(appComboBox.getValue()).get(0));
+                request.setOther(infoComboBox.getValue());
+                requestDialog.close();
+                showDialogConfirm(request);
+            }
+        });
+        content.add(appComboBox, infoComboBox);
+    }
+
+    private void complain(User user, RightRequest request){
+        title.setText("Select App");
+
+        ComboBox<IoTApp> appComboBox= new ComboBox<>("Apps");
+        appComboBox.setItems(dataBaseService.getUserApps(user));
+        appComboBox.setItemLabelGenerator(IoTApp::getName);
+        appComboBox.setPlaceholder("Filter by name...");
+
+        TextArea textArea=new TextArea("Complain");
+        textArea.setPlaceholder("Write your complain");
+        textArea.setWidthFull();
+
+        continueButton.addClickListener(e->{
+            if(appComboBox.getValue()!=null && textArea.getValue()!=null){
+                request.setApp(appComboBox.getValue());
+                request.setReceiver(dataBaseService.getControllersFromApp(appComboBox.getValue()).get(0));
+                request.setOther(textArea.getValue());
+                requestDialog.close();
+                showDialogConfirm(request);
+            }
+        });
+        content.add(new VerticalLayout(appComboBox, textArea));
     }
 
     public void showDialogConfirm(RightRequest request){
@@ -121,7 +186,7 @@ public class DialogRight{
             return;
         }
         Dialog confirmDialog=new Dialog();
-        //confirmDialog.setWidth("50%");
+        confirmDialog.setWidth("50%");
 
         HorizontalLayout appName=new HorizontalLayout(new H1("APP:  "), new H2(request.getApp().getName()));
         appName.setAlignItems(FlexComponent.Alignment.CENTER);
@@ -155,6 +220,18 @@ public class DialogRight{
         if(request.getRightType().equals(RightType.ERASURE)){
             return("Dear " + request.getReceiver().getName() + ", \n" +
                     "I would like to erase the following information: " +request.getOther() + " ,from the app " + request.getApp().getName() + ",\n" +
+                    "Best regards, \n" +
+                    request.getSender().getName());
+        }
+        if(request.getRightType().equals(RightType.INFO)){
+            return("Dear " + request.getReceiver().getName() + ", \n" +
+                    "I would like to know the following information: " +request.getOther() + " regarding the app " + request.getApp().getName() + ",\n" +
+                    "Best regards, \n" +
+                    request.getSender().getName());
+        }
+        if(request.getRightType().equals(RightType.COMPLAIN)){
+            return("Dear " + request.getReceiver().getName() + ", \n" +
+                    "I would like to know the complain with the supervision authority: " +request.getOther() + " about the app " + request.getApp().getName() + ",\n" +
                     "Best regards, \n" +
                     request.getSender().getName());
         }
