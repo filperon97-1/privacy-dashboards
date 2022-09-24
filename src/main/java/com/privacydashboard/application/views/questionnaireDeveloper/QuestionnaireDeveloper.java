f5b937e8e6cc131e63be61ae6b99e5d74657bb47
FILIPPO PERON s284138 <s284138@studenti.polito.it>
2022-09-24 16:24:48 +0200

    Questionnaire più organizzato

--- src/main/java/com/privacydashboard/application/views/questionnaireDeveloper/QuestionnaireDeveloper.java
+++ src/main/java/com/privacydashboard/application/views/questionnaireDeveloper/QuestionnaireDeveloper.java
@@ -1,17 +1,15 @@
 package com.privacydashboard.application.views.questionnaireDeveloper;
 
+import com.vaadin.flow.component.EventData;
 import com.vaadin.flow.component.Html;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.contextmenu.ContextMenu;
 import com.vaadin.flow.component.dependency.NpmPackage;
-import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.Section;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
-import com.vaadin.flow.component.splitlayout.SplitLayout;
 import com.vaadin.flow.component.tabs.Tab;
 import com.vaadin.flow.component.tabs.Tabs;
 import com.vaadin.flow.component.textfield.TextArea;
@@ -27,6 +25,7 @@ import javax.annotation.security.PermitAll;
 @PermitAll
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 public class QuestionnaireDeveloper extends AppLayout {
+    private final Tabs tabs= new Tabs();
     private final Integer nSections= 4;
     private final VerticalLayout[] sections= new VerticalLayout[nSections];
     private final Integer nQuestions= 13;
@@ -36,18 +35,18 @@ public class QuestionnaireDeveloper extends AppLayout {
     private final ContextMenu[] contextMenus= new ContextMenu[nQuestions];
     private final TextArea[] textAreas= new TextArea[nQuestions];
     private final VerticalLayout[] singleQuestion= new VerticalLayout[nQuestions];
+    private final Span content= new Span();
+
+    private Integer k=0;
 
     Logger logger = LoggerFactory.getLogger(getClass());
     public QuestionnaireDeveloper(){
-
-
         // STILE DELLA PAGINA: DA PENSARCI DOPO AVER INDIVIDUATO TUTTE LE DOMANDE DEL QUESTIONARIO E IN CASO AVERLE DIVISE
         // IN SEZIONI
 
         setPrimarySection(Section.DRAWER);
         H1 title= new H1("Questionnaire");
         title.addClassName("title-questionnaire");
-        Tabs tabs= new Tabs();
         for(int i=0; i<nSections; i++){
             Tab tab=new Tab("Section " + Integer.toString(i+1));
             tab.addClassName("pointer");
@@ -55,13 +54,6 @@ public class QuestionnaireDeveloper extends AppLayout {
             sections[i]= new VerticalLayout();
             sections[i].addClassName("section-questionnaire");
         }
-        /*Tabs tabs= new Tabs(new Tab("Section 1"), new Tab("Section 2"), new Tab("Section 3"), new Tab("Section 4"));
-        for(int i=0; i<tabs.getComponentCount(); i++){
-            tabs.setSelectedIndex(i);
-            tabs.getSelectedTab().addClassName("pointer");
-        }*/
-        //tabs.setSelectedIndex(0);
-        //tabs.addClassName("tabs");
         tabs.setOrientation(Tabs.Orientation.VERTICAL);
         //addToNavbar(title,tabs);
         com.vaadin.flow.component.html.Section sectionDrawer= new com.vaadin.flow.component.html.Section(title, tabs);
@@ -69,7 +61,6 @@ public class QuestionnaireDeveloper extends AppLayout {
         //addToDrawer(new VerticalLayout(new Span("AAAAAA"), new Span("AAAAAA"), new Span("AAAAAA"), new Span("AAAAAA")));
         //setResponsiveSteps(new ResponsiveStep("0", 1));
         sectionDrawer.addClassNames("drawer-questionnaire");
-
         // FINE PARTE SULLO STILE
 
         // associate icons with context menu
@@ -84,32 +75,64 @@ public class QuestionnaireDeveloper extends AppLayout {
             contextMenus[i].addClassName("info");
         }
 
-        // SEZIONE 1
-        int k=0;
+        section1();
+        section2();
+        setContent(content);
 
+        tabs.addSelectedChangeListener(e-> changeTab(e));
+        content.add(sections[0]);
+
+        /*
+        //Domanda X:
+        titles[k]= new Div(new Span(""), icons[k]);
+        contextMenus[k].addItem("");
+        radioGroups[k]= new RadioButtonGroup<>();
+        radioGroups[k].setItems();
+        k++;
+         */
+
+        /*
+        VerticalLayout internalLayout= new VerticalLayout();
+        internalLayout.addClassName("internalLayout-questionnaire");
+        for(int i=0; i<nQuestions; i++){
+            singleQuestion[i]=new VerticalLayout(titles[i], radioGroups[i]);
+            singleQuestion[i].addClassName("singleQuestion-questionnaire");
+            if(textAreas[i]!= null){
+                textAreas[i].addClassName("textArea-questionnaire");
+                singleQuestion[i].add(textAreas[i]);
+            }
+            internalLayout.add(singleQuestion[i]);
+        }
+        setContent(internalLayout);*/
+    }
+
+    // SEZIONE 1
+    private void section1(){
+        int begin=k;
         //Domanda 1: Does the app transfer data to a third party?
-        titles[0]= new Div(new Span("Does the app transfer data to a third party?"), icons[0]);
-        contextMenus[0].addItem(createInfo("GDPR Article 13", "Where personal data relating to a data subject are collected from the data subject, the controller shall, at the time when personal data are obtained, provide the data subject with... the fact that the controller intends to transfer personal data to a third country or international organisation"));
-        radioGroups[0]= new RadioButtonGroup<>();
-        radioGroups[0].setItems("No", "Yes, only in United Europe", "Yes, also outside United Europe", "I don't know");
-        textAreas[0]= new TextArea("List which third countries, if any");
-
+        titles[k]= new Div(new Span("Does the app transfer data to a third party?"), icons[0]);
+        contextMenus[k].addItem(createInfo("GDPR Article 13", "Where personal data relating to a data subject are collected from the data subject, the controller shall, at the time when personal data are obtained, provide the data subject with... the fact that the controller intends to transfer personal data to a third country or international organisation"));
+        radioGroups[k]= new RadioButtonGroup<>();
+        radioGroups[k].setItems("No", "Yes, only in United Europe", "Yes, also outside United Europe", "I don't know");
+        textAreas[k]= new TextArea("List which third countries, if any");
+        k++;
 
         //Domanda 2: Periodo mantenimento dei dati
-        titles[1]= new Div(new Span("For how long are the data going to be stored?"), icons[1]);
-        contextMenus[1].addItem(createInfo("GDPR Article 13", "the controller shall, at the time when personal data are obtained, provide the data subject with the following further information... the period for which the personal data will be stored"));
-        radioGroups[1]= new RadioButtonGroup<>();
-        radioGroups[1].setItems("less than 1 month", "between 1 month and 6 months", "between 6 months and 2 years", "more than 2 years", "I don't know");
+        titles[k]= new Div(new Span("For how long are the data going to be stored?"), icons[1]);
+        contextMenus[k].addItem(createInfo("GDPR Article 13", "the controller shall, at the time when personal data are obtained, provide the data subject with the following further information... the period for which the personal data will be stored"));
+        radioGroups[k]= new RadioButtonGroup<>();
+        radioGroups[k].setItems("less than 1 month", "between 1 month and 6 months", "between 6 months and 2 years", "more than 2 years", "I don't know");
+        k++;
 
         //Domanda 3: Esistenza processo decisionale automatizzato
-        titles[2]= new Div(new Span("Is there an automated decision-making, including profiling?"), icons[2]);
-        contextMenus[2].addItem(createInfo("GDPR Article 13", "the controller shall, at the time when personal data are obtained, provide the data subject with the following further information... the existence of automated decision-making, including profiling"));
-        radioGroups[2]= new RadioButtonGroup<>();
-        radioGroups[2].setItems("Yes", "No", "I don't know");
+        titles[k]= new Div(new Span("Is there an automated decision-making, including profiling?"), icons[2]);
+        contextMenus[k].addItem(createInfo("GDPR Article 13", "the controller shall, at the time when personal data are obtained, provide the data subject with the following further information... the existence of automated decision-making, including profiling"));
+        radioGroups[k]= new RadioButtonGroup<>();
+        radioGroups[k].setItems("Yes", "No", "I don't know");
+        k++;
 
         //Domanda 4: Che dati vengono processati?
 
-        k=3;
         //Domanda X: Hai individuato i dati che verranno processati e in che modo lo saranno?
         titles[k]= new Div(new Span("Have you identified all the data that are going to be processed?"), icons[k]);
         contextMenus[k].addItem("");
@@ -152,6 +175,19 @@ public class QuestionnaireDeveloper extends AppLayout {
         radioGroups[k].setItems("Yes", "No", "I don't know");
         k++;
 
+        for(int i=begin; i<k; i++){
+            singleQuestion[i]=new VerticalLayout(titles[i], radioGroups[i]);
+            singleQuestion[i].addClassName("singleQuestion-questionnaire");
+            if(textAreas[i]!= null){
+                textAreas[i].addClassName("textArea-questionnaire");
+                singleQuestion[i].add(textAreas[i]);
+            }
+            sections[0].add(singleQuestion[i]);
+        }
+    }
+
+    private void section2(){
+        int begin=k;
         //SEZIONE 2
 
         // Domanda 1: Usi pseudonomizzazione?
@@ -185,25 +221,24 @@ public class QuestionnaireDeveloper extends AppLayout {
         radioGroups[k].setItems("every week", "between a week and a month", "between a month and a year", "more than a year", "never", "I don't know" );
         k++;
 
-        //Domanda X:
-        titles[k]= new Div(new Span(""), icons[k]);
-        contextMenus[k].addItem("");
-        radioGroups[k]= new RadioButtonGroup<>();
-        radioGroups[k].setItems();
-        k++;
-
-        VerticalLayout internalLayout= new VerticalLayout();
-        internalLayout.addClassName("internalLayout-questionnaire");
-        for(int i=0; i<nQuestions; i++){
+        for(int i=begin; i<k; i++){
             singleQuestion[i]=new VerticalLayout(titles[i], radioGroups[i]);
             singleQuestion[i].addClassName("singleQuestion-questionnaire");
             if(textAreas[i]!= null){
                 textAreas[i].addClassName("textArea-questionnaire");
                 singleQuestion[i].add(textAreas[i]);
             }
-            internalLayout.add(singleQuestion[i]);
+            sections[1].add(singleQuestion[i]);
         }
-        setContent(internalLayout);
+    }
+
+    private void changeTab(Tabs.SelectedChangeEvent e){
+        if(e.getSelectedTab().equals(e.getPreviousTab())){
+            return;
+        }
+        content.removeAll();
+        content.add(sections[tabs.getSelectedIndex()]);
+
     }
 
     private Html createInfo(String title, String description){
